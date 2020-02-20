86733d9028c6b665cf15e165d7442b7aa0cbd3aa
kasselTrankos <alvaro.touzon@gmail.com>
2020-02-20 21:29:29 +0100

    feat: - run recursive

--- fp/monad/task.js
+++ fp/monad/task.js
@@ -9,6 +9,58 @@ Task.of = function(x){
   return new Task((_, resolve) => resolve(x));
 }
 
+// chain :: Chain m => m a ~> ( a -> m b) ->  m b
+Task.prototype.chain = function(f) {
+  return new Task((reject, resolve) => {
+    this.fork(a=> reject(a), a => f(a).fork(reject, resolve)),
+    this.cleanup
+  });
+} 
+
+// app :: Apply f => f a ~> f(a->b) -> f b
+Task.prototype.ap = function(that) {
+  const _thisFork = this.fork;
+  const _thatFork = that.fork;
+  let fn, fLoaded = false;
+  let v, vLoaded = false;
+  let cleanupThis = this.cleanup;
+  let cleanupThat = that.cleanup;
+  let rejected = false;
+  function cleanupBoth(state) {
+    cleanupThis(state[0]);
+    cleanupThat(state[1]);
+  }
+  return new Task((reject, resolve) => {
+    const guardReject = x => {
+      if (!rejected) {
+        rejected = true;
+        return reject(x);
+      }
+    }
+    const guard = setter => {
+      return x => {
+        setter(x);
+        if(fLoaded && vLoaded) {
+          delayed(function(){ cleanupBoth(allState) });
+          return resolve(fn(v));
+        }else {
+          return x;
+        }
+      }
+    }
+    const _this = _thisFork(guardReject, guard(x => {
+      vLoaded = true;
+      v = x;
+    }));
+    const _that = _thatFork(guardReject, guard(x => {
+      fLoaded = true;
+      fn = x;      
+    }));
+    
+    return allState = [_this, _that];
+  }, this.cleanup);
+}
+
 // map :: Functor => f a ~> (a -> b) -> f b
 Task.prototype.map = function(f) {
   return new Task((reject, resolve)=> {
--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 const {Task} = require('./fp/monad');
 const {readdir} = require('fs');
-const {pipe, map, lift} = require('ramda');
+const {pipe, map, lift, chain} = require('ramda');
+const lift2 =(f, a, b) => console.log(f, a, b) ||  a.ap(b.map(f))
 
-const ReaderTask = dir => new Task((reject, resolve)=> {
+const ReaderTask = dir => console.log(dir, '1231234234') ||  new Task((reject, resolve)=> {
   readdir(dir, function(err, list) {
     return err ? reject(err) : resolve(list);
   });
 });
-const append = x => xs => [x, ...xs];
+const append = x => xs => console.log(x, '0123213') || [x, ...xs];
 
 
-const insideOut = (T, xs) => xs.reduce(
-  (acc, x) => lift(append, x, acc),
-  T.of([]))
+const path = x => `./node_modules/${x}/node_modules`;
+const insideOut = (T, xs) => console.log(xs) ||  xs.reduce(
+  (acc, x) => lift2(append, ReaderTask(path(x)), acc), T.of([]));
 
 const paralellize = users =>
 insideOut(Task, users.map(API.getById))
 const toUpperCase = v => v.toUpperCase();
-const concat = path => insideOut(Task,  path.map(x => x));
+const concat = xs => insideOut(Task, xs)
 
 const program = pipe(
   ReaderTask,
-  map(map(toUpperCase)),
-  map(concat)
+  // map(map(toUpperCase))
+  chain(concat)
 );
 
 program('./node_modules')
-  .fork(console.log, (data) => console.log(data) );
+  .fork(c => console.error('00d0dd', c), (data) => console.log('0das0dassad', data));
