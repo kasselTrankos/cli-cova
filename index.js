fc16e82d5a96efb7f0574355ecc68368fe46a5ad
kasselTrankos <alvaro.touzon@gmail.com>
2020-02-20 22:39:50 +0100

    reflextion use chain + reduce ??, so what is this

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const {Task} = require('./fp/monad');
 const {readdir} = require('fs');
-const {pipe, map, lift, chain, ap} = require('ramda');
-const lift2 =(f, a, b) => b.ap(a.map(f))
+const {pipe, map, chain, ap} = require('ramda');
+const lift2 =(f, a, b, c) => c.ap(b.ap(a.map(f)))
 
 const ReaderTask = dir => new Task((reject, resolve)=> {
   readdir(dir, function(err, list) {
     return err ? reject(err) : resolve(list);
   });
 });
-const append = x => xs => [x, ...xs];
+const append = x => y => xs => [x, y, ...xs];
 
 
-const path = x => `./node_modules/${x}/dist`;
+const path = x => `/home/vera/Documentos/${x}/fp`;
 
 // aqui va la recusividad
 const redus = xs => xs.reduce((acc, x)=> lift2(append, Task.of(x), ReaderTask(path(x)), acc), Task.of([]))
@@ -19,9 +19,9 @@ const redus = xs => xs.reduce((acc, x)=> lift2(append, Task.of(x), ReaderTask(pa
 const program = pipe(
   ReaderTask,
   chain(redus),
-  map(x=> x.flatMap(x=> x))
+  // map(x=> x.flatMap(x=> x))
   
 );
 
-const h = program('./node_modules')
+const h = program('/home/vera/Documentos')
   .fork(c => console.error('00d0dd', c), (data) => console.log('0das0dassad', data));
