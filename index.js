9ee2bb232a87c89a21214e2f5d29ef7a205d2c2f
kasselTrankos <alvaro.touzon@gmail.com>
2020-03-16 22:19:19 +0100

    feat: - map map

--- index.js
+++ index.js
@@ -15,51 +15,50 @@ const toTask = maybe =>  maybe.cata({
 const toArray = maybe => maybe.cata({
   Just: x => [x],
   Nothing: () => []
-
 });
+const toMaybe = x => Just(x);
 const lift2 = (f, a, b, c) => c.ap(b.ap(a.map(f)))
 const lift = (f, a, b) => b.ap(a.map(f));
 const appends = x => xs => [...x, ...xs];
 // const just = compose(liftF, Just)
 // const nothing = liftF(Nothing)
 const isDir = path => statSync(path).isDirectory() ? Just(path) : Nothing;
-const exists = path => existsSync(path) ? Just(path) : Nothing;
+const exists = path => console.log(path, '000000') ||  existsSync(path) ? Just(path) : Nothing;
 const read = dir => new Task((reject, resolve)=> {
-  console.log('DIR', dir)
   readdir(dir, function(err, list = []) {
-    console.log('LIST', list)
     return err ? reject(err) : resolve(list.map(x => `${dir}/${x}`));
   });
 });
 // aqui va la recusividad
 const readDir = xs => console.log(xs, 'eeems') || xs.reduce((acc, x) => lift(appends, read(x), acc), Task.of([]));
-const getHead = xs => {
-  const [head, tail] = xs;
-  console.log(head, tail);
-  return head;
-}
-// const readDir = xs =>  console.log('rere --->' , xs) || xs.reduce((acc, x)=> lift(append, read(`${x}/node_modules`), acc), Task.of([]));
-  
-const its = ['./node_moduleds', '/Users/aotn'];
+const DIRS = ['./node_modules', '/Users/aotn', '/Users/aotn/GEMA/components/'];
 // const traverse = xs => xs.reduce((acc, x) => lift2(append, Task.of(x), read(x), acc), Task.of([]));
 const car = pipe(
   compose(chain(isDir), exists),
   toTask,
-  // toArray,
   // readDir,
+  );
+const onlyDir = pipe(
+  compose(chain(isDir), exists),
+  toArray
 );
 const truck = pipe(
   chain(x => console.log(x, 'aaaaaaaa') || car(x)),
   map(x => console.log(x, '00000000') || x),
 );
+const ff = x => Task.of(x.map(onlyDir));
 
 // caminon de camniones
 const program = pipe(
   map(car),
+  map(chain(readDir)),
+  map(map(ff))
+  // map(ap(Task.of(car))),
+  // map(readDir)
   // map(ap(Task.of(2))),
   // getHead,
 );
-const k = program(its);
+const k = program(DIRS);
 k.map(t => t.fork(d => console.log('error', d), x => console.log('gol', x)))
   // .fork(console.log, console.log)
-// console.log(k, '000000000')
+console.log(k, '000000000')
