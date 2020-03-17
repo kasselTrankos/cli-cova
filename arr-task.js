7eeec0964d69c13fb5f0da21d5794f97e8e53cb7
kasselTrankos <alvaro.touzon@gmail.com>
2020-03-17 20:54:48 +0100

    feat: - next trunk

--- arr-task.js
+++ arr-task.js
@@ -4,45 +4,52 @@ const path = require("path");
 const {pipe, map, chain, ap, curry} = require('ramda');
 const {readdir, statSync, existsSync} = require('fs');
 
-const DIRS = ['./node_modules', '/Users/aotn', '/Users/aotn/GEMA/components'];
-
+const DIRS = ['./node_modules'];
 const {Just, Nothing} = Maybe;
+
+// :: Maybe -> Task
 const toTask = maybe => maybe.cata({
   Just: x => Task.of(x),
   Nothing: () => Task.of([])
 });
-const read = dir => console.log('00000000', dir) || new Task((reject, resolve)=> {
+// :: String -> Task
+const read = dir => new Task((reject, resolve)=> {
   readdir(dir, function(err, list = []) {
     return err ? reject(err) : resolve(list.map(x => `${dir}/${x}`));
   });
 });
 const appends = x => xs => [...x, ...xs];
 const lift2 = (f, a, b) => b.ap(a.map(f));
-
+// :: String -> Maybe
 const isDir = path => statSync(path).isDirectory() ? Just(path) : Nothing;
+
+// :: String -> Maybe
 const isHidden = p => /^\./.test(path.basename(p)) ? Nothing : Just(p);
+
+// :: String -> Maybe
 const exists = path => existsSync(path) ? Just(path) : Nothing;
-const traverse = (T, xs) => xs.reduce((acc, x) => lift2(appends, Task.of(x),  acc), T.of([]));
+// const traverse = (T, xs) => xs.reduce((acc, x) => lift2(appends, Task.of(x),  acc), T.of([]));
 const sequence = (T, xs) => xs.reduce((acc, x) => lift2(appends, x,  acc), T.of([]));
-// car
-//   :: [String] -> Task e [DIR]
-const car = (T, dirs) =>
-	sequence(T, dirs.map(clearData))
 
-
-const arrToTask = curry(car);
-const clearData = pipe(
-  exists,
-  chain(isDir),
+const valid = pipe(
+	exists,
+	chain(isDir),
 	chain(isHidden),
 	toTask,
-	chain(read)
-	// chain(map(read)),
+);
+const folder = pipe(
+	valid,
+	chain(read),
 ); 
+//  :: [String] -> Task e [DIR]
+const car = (T, dirs) =>
+	sequence(T, dirs.map(folder))
+
+const bicycle = curry(car);
 
 
 const program = pipe(
-	arrToTask(Task),
+	bicycle(Task),
 	// map(map(x=> x+ 'jjfifjif'))
 	// chain(map(clearData)),
 	// map(x => console.log(x, '4444444444444'))
