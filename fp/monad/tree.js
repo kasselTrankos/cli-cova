e1c2cacc7b81d79ccca5350c035c5c183444d12c
kasselTrankos <alvaro.touzon@gmail.com>
2020-03-19 22:07:09 +0100

    feat: custom specific reduce

--- fp/monad/tree.js
+++ fp/monad/tree.js
@@ -14,14 +14,13 @@ RoseTree.prototype.ap = function({node: f, forest: fs}) {
   const {node, forest} = this;
   return new RoseTree(f(node), [].concat(
     forest.map(x => x.map(f)),
-    fs.map(m => this.node.ap(m))
+    fs.map(m => this.ap(m))
   ));
 }
 // chain :: Cahin m => m a ~> (a -> m b)-> m b
 RoseTree.prototype.chain = function(f) {
   const { node: x, forest: xs } = f(this.node);
-  return new RoseTree(x, [].concat(xs,
-    this.forest.map(x => x.chain(f))
+  return new RoseTree(x, [].concat(xs, this.forest.map(x => x.chain(f))
   ))
 }
 // map :: Functor f => f a ~> (a -> b) -> f b
@@ -31,6 +30,10 @@ RoseTree.prototype.map = function(f) {
 // concat :: Semogroup a => a ~> a -> a
 RoseTree.prototype.concat = function(b) {
   return new RoseTree(this.node, [].concat(this.forest, b))
+}
+// reduce :: Foldable f => f a ~> ((b, a) -> b, b) -> b
+RoseTree.prototype.reduce = function (f, acc) {
+  return this.forest.reduce((acc, rt) => rt.reduce(f, acc), f(acc, this.node))
 }
 
 module.exports = RoseTree;
--- test-tree.js
+++ test-tree.js
@@ -1,31 +1,29 @@
 const {RoseTree} = require('./fp/monad');
-const {pipe, map, chain, ap, curry} = require('ramda');
-
 const lift2 = (f, a, b) => b.ap(a.map(f));
 const append = x => xs => [x, ...xs];
-const traverse = (T, xs) => xs.reduce((acc, x) => lift2(append, T.of(x), acc), T.of([]));
-// const convert = traverse(RoseTree, [1, 2, 3]);
-// 
-// console.log(convert, '1111111111')
-// const t = RoseTree.of();
-const D = [1, 2, 3];
-const c = RoseTree.of(1);
-const v = traverse(RoseTree, D);
-const a = RoseTree.of(1);
-const b = new RoseTree(x=> x.node ? x.node + 110 : x + 10, [ RoseTree.of(x => x + 1000) ]);
-const g = new RoseTree(c, [a]).ap(b)//.ap(d);//.map(x=> x.node + 10);
-const m = c.chain(x => new RoseTree(x, [RoseTree.of(2)]))//.chain(x => new RoseTree(x + 3, [RoseTree.of(100)]));
-// console.log(m);
-const f = [1,2,3,4].reduce((acc, x)=> acc.chain(a => new RoseTree(x, [RoseTree.of(1)])), RoseTree.of(1))
-// console.log(g, f, m);
-const a1 = RoseTree.of(1);
-const a2= RoseTree.of(2);
-const a3= RoseTree.of(3);
-const cc = a1.concat(a2.concat(a3));
-console.log(cc);
-//.ap(new RoseTree(RoseTree.of(x=> x +1), [a]));
-// chain will merge
-// console.log(c, '<------00000000', v);
-// console.log(v)
-// const gg = new RoseTree(1, [new RoseTree(2, [new RoseTree(3, [])], [])])
-// console.log(gg)
+const sequence = (T, xs) => xs.reduce((acc, x) => lift2(append, T.of(x),  acc), T.of([]));
+
+
+const tree = new RoseTree(1, [RoseTree.of(2), new RoseTree(3, [RoseTree.of(4)])]);
+const tree_1 = new RoseTree(1, [RoseTree.of(2), RoseTree.of(90)])
+
+console.log(tree.map(x=> x.node || x));
+const b = tree_1.ap(new RoseTree(x=> x +10, [RoseTree.of(x  => x +1290)]))
+console.log(b);
+
+// const example = new RoseTree( 1, [
+//   new RoseTree(2, [RoseTree.of(9), new RoseTree(3, [
+//     new RoseTree(4, [])
+//   ])]),
+  
+// ]);
+const example = new RoseTree(1, [new RoseTree(2, [new RoseTree(3, [RoseTree.of(4)]), new RoseTree(5, [RoseTree.of(12)])])]);
+
+const reduce = xs => {
+  const red = (acc, x) => x.forest.reduce(red, [...acc, x.node]);
+
+  return xs.forest.reduce(red, [xs.node]);
+} 
+const n = example.reduce((acc, x)=> console.log(x, acc.concat) || [...acc, x], []);
+console.log(n, '1111111111', reduce(example));
+
