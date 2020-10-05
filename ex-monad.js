882eb93dabcf6c374914e217affecb2fb398ff60
kasselTrankos <alvaro.touzon@gmail.com>
2020-10-05 21:32:19 +0200

    feat: add alt for error, great Tom

--- ex-monad.js
+++ ex-monad.js
@@ -1,21 +1,9 @@
 const { Maybe, Task } = require('./fp/monad');
 const daggy = require('daggy');
-const { generator } = require('jsverify');
 const pipe = (...fns) => x => fns.reduceRight((acc, fn)=> fn(acc) , x);
-const lift = (f, a, b) => a.map(f).ap(b);
 const map = f => xs => xs.map(f);
 const chain = f => xs => xs.chain(f);
 
-// console.log(Just(3).alt(Nothing))
-// console.log(Nothing.alt(Just(10)).alt(Just(9000)));
-
-// console.log(Nothing.cata);
-const gg = new Task((reject, resolve)=> {
-    setTimeout(()=> resolve('op'), 100);
-});
-const gg1 = new Task((rejecgat, resolve)=> {
-    setTimeout(()=> resolve('op222'), 100);
-});
 
 
 const Animal = daggy.taggedSum('Animal', {
@@ -58,34 +46,25 @@ Animal.prototype.alt = function(b) {
         Dead: _=>  b
     });
 }
-const sequence = xs =>xs.reduce((acc, x) => lift(appends, x, acc), Task.of([]));
-const appends = x => xs => [...xs, x];
-const g = Animal.Alive([12, 121]);
-const m = Animal.Alive(1);
-const ids = [1, 2, 3, 4];
+const lift = (f, a, b) => a.map(f).ap(b);
+const sequence = xs =>xs.reduce((acc, x) => lift(appends, x.alt(Task.of([])), acc), Task.of([]));
+const appends = x => xs => [...xs, ...x];
 const getIds = x => new Task((reject, resolve)=> {
-    setTimeout(()=> resolve(x + 900), 100);
+    setTimeout(()=>{
+        return (x === 3) 
+        ? reject([])
+        : resolve([x + 900])
+    }, 90)
 });
 
-
-const ga = x => new Task((reject, resolve) => {
-    setTimeout(()=> resolve(x +'-->es el x'), 200);
-});
-
-const a = ga(1);
-const b = ga(2)
-
-const c = a.map(x => x).ap(b);
-const ba = Task.of([1, 2])
-
-
-const proc = pipe(
-    chain(ga)
-);
 const proc1 = pipe(
     sequence,
     map(getIds)
 );
-proc1(ids)
+const er = new Task((reject, resolve)=> reject(90));
+
+proc1([1,2,3,4])
+    .fork(console.error, console.log);
+
+er.alt(getIds(9))
     .fork(console.error, console.log)
-//proc(ba).fork(console.error, console.log)
