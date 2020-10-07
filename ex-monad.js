b4f45abd431f78b111c4530162437b26fa4722db
kasselTrankos <alvaro.touzon@gmail.com>
2020-10-07 16:08:20 +0200

    feat: start with []

--- ex-monad.js
+++ ex-monad.js
@@ -1,11 +1,51 @@
 const { Maybe, Task } = require('./fp/monad');
 const daggy = require('daggy');
 const {readdir, stat, exists} = require('fs');
-const { I } = require('./lambda');
-const { append } = require('sanctuary');
+
+
+
+const Animal = daggy.taggedSum('Animal', {
+  Alive : ['v'],
+  Dead : []
+});
+
+
+
+
+// map :: Functor a => f a -> (a -> b) ~> b
+Animal.prototype.map = function(f) {
+  return this.cata({
+    Alive: v => Animal.Alive(f(v)),
+    Dead:() => Animal.Dead
+  });
+}
+
 /// quit from context???
 /// mmm easy with chain?
 
+// flatmap :: + f a => a -> (a -> b) -> b
+Animal.prototype.flatmap = function(f) {
+  return this.map(f).cata({
+    Alive: x => x,
+    Dead: () => Animal.Dead
+  });
+}
+
+// chain :: Chain m => m a ~> (a -> m b) -> m b4
+Animal.prototype.chain = function(b) {
+  return this.cata({
+    Alive: v => Animal.Alive(b.v(v)),
+    Dead: Animal.Dead
+  });
+}
+
+// alt :: Alt f => f a ~> f a -> f a
+Animal.prototype.alt = function(b) {
+  return this.cata({
+    Alive: _=> this,
+    Dead: _=>  b
+  });
+}
 const isDirectory = s => new Task((reject, resolve)=> {
   stat(s, (err, stats) => { 
     if (err)  return reject([]); 
@@ -21,69 +61,24 @@ const chain = f => xs => xs.chain(f);
 const empty = Task.of([]);
 const _default = x => x.alt(empty);
 const lift = (f, a, b) => a.map(f).ap(b);
-const sequence = xs =>  xs.reduce((acc, x) => lift(appends, _default(x), acc), empty);
-const sequence1 = xs => xs.reduce((acc, x) => lift(appends, _default(x), acc), empty);
+const sequence = xs =>xs.reduce((acc, x) => lift(appends, _default(x), acc), empty);
 const appends = x => xs => [...xs, ...x];
-  const read = s => new Task((reject, resolve) => readdir(s, (err, files) =>  
-    err ? reject([]) : resolve(files.map(c => `${s}${c}/`))
-));
-
-const gp = s => console.log(s, '1111111111') || Task.of(s);
-const nolo = c => c + '0000';
-const log = s => x => {
-  console.log(`LOG(${s}):::`, x);
-  return x;
-}
+const read = s => new Task((reject, resolve) => {
+  readdir(s, (err, files) => { 
+    return (err) ? reject([]) : resolve(files.map(c => `${s}${c}/`));
+  });
+});
 const proc1 = pipe(
-  // map(proc2),
-  // chain(xs => Task.of(proc2(xs))),
-  map(map(read)),
-  map(log('huevo')),
-  map(I),
+  sequence,
+  map(isDirectory)
 );
-const proc = pipe(
-  chain(xs => Task.of(sequence1(xs))),
-  map(isDirectory),
-    
+const proc2 = pipe(
+  chain(proc1),
+  read
 );
-// function proc1(x) {
-//   pipe(
-//     sequence,
-//     map(isDirectory)
-//   )(x);
-// }
-const seq = chain(xs => Task.of(sequence(xs)));
-function proc2 (x) {
-  log('9999')(x)
-  return pipe(
-    // proc1,
-    // seq,
-    // map(chain(proc)),
-    // chain(xs => Task.of(xs.map(read)))
-    // map(sequence),
-    // map(map(read)),
-    // map(sequence),
-    seq,
-    chain(c => Task.of(c.map(read))),
-    map(map(log('0000111111'))),
-  )(x);
-}
-// const proc2 = pipe(
-//   chain(proc1),
-//   read
-// );
 const er = new Task((reject, resolve)=> reject(90));
-const on = xs => xs.reduce((acc, x)=> appends(_default(x))(acc), Task.of([]));
-const dirs = ['./fp', './fp/monad', './0', './node_modules', './fp/mal'];
-const f = ['./']
-const t = Task.of(f);
-const p = pipe(
-  chain(I),
-  // map(map(isDirectory)),
-  chain(xs => Task.of(sequence(xs))),
-  // map(map(map(map(isDirectory)))),
-  map(map(read))
-);
-
-p(t)
-  .fork(console.error, log('0'))
+const dirs = ['./fp', './fp/monad', './0', './node_modules', './fp/mal']
+proc2('./')
+  .fork(console.error, console.log);
+// proc1(dirs)
+//     .fork(console.error, console.log);
