99dabd9ade8a2152e2b1f2896a1bce7c97dbe8f1
kasselTrankos <alvaro.touzon@gmail.com>
2021-01-29 01:07:35 +0100

    feat: clean d.js

--- d.js
+++ d.js
@@ -21,18 +21,23 @@ const Url = $.NullaryType
   (x => Object.prototype.toString.call (x) === '[object URL]');
 
 const S = sanctuary.create ({
-    checkTypes: true, 
-    env: sanctuary.env.concat(env).concat([Url])})
+  checkTypes: true, 
+  env: sanctuary.env.concat(env).concat([Url])
+})
 const { pipe, Either, chain, map, ap, pipeK, filter } = S
+
+const dbName = 'crawl'
+const tbl = 'links'
+
 
 // safeURL -> String -> {} -> Either {} String
 const safeURL = x => {
-    try {
-      return S.Right(new URL(x))
-    } catch (e) {
-      return S.Left(' no es una URL válida ' + x)
-    }
+  try {
+    return S.Right(new URL(x))
+  } catch (e) {
+    return S.Left(' no es una URL válida ' + x)
   }
+}
 
 
 
@@ -45,30 +50,10 @@ const eitherToFuture = S.either (Future.reject) (Future.resolve)
 // safepath :: String -> {} -> Either String *
 const safepath = s => o => reduce((acc, x) => chain (safeprop(x)) (acc),  S.Right(o), s.split('.'))
 
-
-
-
-
-const getCharacter = id => encaseP(fetch)(`https://www.breakingbadapi.com/api/characters/${id}`)
-.pipe(Future.chain(encaseP(r => r.text())))
-
-
-// getTableName :: String -> Future e String
-const getTableName = pipe([
-  listCollections,
-  chain(pipe([
-    safepath('0.name'),
-    eitherToFuture,
-  ])),
-])
-const proc = pipe([
-  S.of(Future),
-  chain(a => chain (b => find (a) (b) ({})) (getTableName(a)))
-  //   a =>  S.lift2(x => y => `[${x}] + ${y}`)  
-  //     (resolve(a))
-  //     (getTable(a))
-])
+// isAbsolutePath :: String -> Boolean
 const isAbsolutePath = x => x.substring(0,1) === '/'
+
+// isFullPath :: String -> Boolean
 const isFullPath = hostname => x => x.includes(hostname)
 
 // href :: Cheerio -> HTML  
@@ -76,31 +61,18 @@ const href = $ => $('a')
 
 // toArray :: Cheerio -> []
 const toArray = $ => $.toArray()
+
 // attr :: String -> Cheerio -> String
 const attr = att => $ => $.attr(att)
 // unique -> [*] -> [*]
+
 const unique = x => [ ... new Set(x) ]
-//
 
-const dbName = 'crawl'
-const tbl = 'links'
-const allLinks = safeprop('0.links')
-
-// const obtainLink = x => console.log(x[0].links, 'lousos') || 
-//   x[0].links ? S.Right([...x[0].links, x[0].url]) : S.Left([])
-
-// const findLinks = x => S.reduce(acc => x => S.concat (acc) (obtainLink(x))) (S.Just([])) (x)
-
-
-const links = pipe([
-  flatten,
-  S.reduce(acc => x => S.concat(acc)(x.links)) ([]),
-  unique,
-  S.of(Future),
-])
-
+/// review names
 const form = ({origin}) => x => isFullPath(origin)(x) ? x :  S.concat (origin) (x)
 const validLink = ({hostname}) => x => isFullPath(hostname)(x) ||  isAbsolutePath(x)
+
+// getLinks -> URL -> Strinh -> [ String ]
 const getLinks = seed => pipe([
   cheerio.load,
   href,
@@ -113,17 +85,17 @@ const getLinks = seed => pipe([
 const load = x => encaseP(fetch)(x)
 .pipe(Future.chain(encaseP(r => r.text())))
 
+
+// safeload :: URL -> Furure e String
 const safeload = pipe([
   safeprop('href'),
   eitherToFuture,
   chain(load),
 ])
 
-const findlink = x =>  find(dbName)(tbl)({url: x})
-
 // safefindlink :: String -> Future error [ String ]
 const safefindlink = pipe([
-  findlink,
+  url => find(dbName) (tbl) ({url}),
   chain(pipe([
     safepath('0.links'),
     eitherToFuture,
@@ -131,10 +103,7 @@ const safefindlink = pipe([
 ])
 
 const loadlinks = x => chain (pipe([ getLinks(x), resolve]) ) (safeload(x))
-const insertlinks = pipeK([
-  loadlinks,
-  // links => chain( chain (S.of(Future) (links) ) (insert(dbName) (tbl) (links)) ) (links)
-])
+
 
 // safeloadlinks :: String -> Future error [ String ]
 const safeloadlinks = pipe([
@@ -143,31 +112,26 @@ const safeloadlinks = pipe([
   chain(url =>  chain   (links => chain( ()=> S.of(Future) (links) )   (insert (dbName) (tbl) ({links, url: url.href}))    )  ( loadlinks(url) ))     ,
 ])
 
+// obtainLinks:: String -> Future error [ String ]
 const obtainLinks = pipe([
   a => S.alt (safeloadlinks(a)) (safefindlink(a))
 ])
-const proc1 = pipe([
-    ask,
-    chain(pipe([
-      safeURL,
-      eitherToFuture,
-    ])),
-    chain(pipe([
-      safeprop('href'),
-      eitherToFuture,
-
-    ])),
-    // eitherToFuture,
-    // map(x => console.log(x, 'zero') || x)
-    chain(c => S.traverse (Future) (obtainLinks) ([c])),
-    map( pipe( [ flatten,  unique ])),
-    chain( S.traverse (Future) (obtainLinks)),
-    map( pipe( [ flatten,  unique ])),
-    chain( S.traverse (Future) (obtainLinks)),
-    map( pipe( [ flatten,  unique ])),
-    // chain(a => chain (Future.resolve (getLinks(a)) ) (load(a) )),
-    // chain(eitherToFuture)
+const proc = pipe([
+  ask,
+  chain(pipe([
+    safeURL,
+    eitherToFuture,
+  ])),
+  chain(pipe([
+    safeprop('href'),
+    eitherToFuture,
+  ])),
+  chain(c => S.traverse (Future) (obtainLinks) ([c])),
+  map( pipe( [ flatten,  unique ])),
+  chain( S.traverse (Future) (obtainLinks)),
+  map( pipe( [ flatten,  unique ])),
+  chain( S.traverse (Future) (obtainLinks)),
+  map( pipe( [ flatten,  unique ])),
 ])
-// https://miqueridowatson.com
-fork (log('00000000')) (log('222222')) (proc1(' dame algo'))
-// fork (log('00000000')) (log('222222')) (proc1(resolve('https://miqueridowatson.com/post-work/sorteos-once/')))
+
+fork (log('00000000')) (log('222222')) (proc(' dame algo'))
